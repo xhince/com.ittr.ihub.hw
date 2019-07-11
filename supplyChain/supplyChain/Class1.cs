6b34d9de8c5cc69d1166aa72e3bc920e682fe6d1
xhince <harika.ince@gmail.com>
2019-07-11 11:24:55 +0300

    supplyChain
    OOP2 HW

---
+++ supplyChain/supplyChain/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supplyChain
+{
+    class supply
+    {
+        private int siparisNo;
+        private int urunId;
+        private int urunAdet;
+        private double urunDesi;
+        private int tedarikciId;
+        private int depoId;
+        private double imalatMaliyeti;
+        private double depoMaliyeti;
+        private double tasimaMaliyeti;
+        private double urunToplamMaliyeti;
+
+        public int SiparisNo
+        {
+            get { return siparisNo; }
+            set { siparisNo = value; }
+        }
+
+        public int UrunId
+        {
+            get { return urunId; }
+            set { urunId = value; }
+        }
+
+        public int UrunAdet
+        {
+            get { return urunAdet; }
+            set { urunAdet = value; }
+        }
+
+        public double UrunDesi
+        {
+            get { return urunDesi; }
+            set { urunDesi = value; }
+        }
+
+        public int TedarikciId
+        {
+            get { return tedarikciId; }
+            set { tedarikciId = value; }
+        }
+
+        public int DepoId
+        {
+            get { return depoId; }
+            set { depoId = value; }
+        }
+
+        public double ImalatMaliyeti
+        {
+            get { return imalatMaliyeti; }
+            set { imalatMaliyeti = value; }
+        }
+
+        public double DepoMaliyeti
+        {
+            get { return depoMaliyeti; }
+            set { depoMaliyeti = value; }
+        }
+         public double TasimaMaliyeti
+        {
+            get { return tasimaMaliyeti; }
+            set { tasimaMaliyeti = value; }
+        }
+        public double UrunToplamMaliyeti
+        {
+            get { return urunToplamMaliyeti; }
+            //set { urunToplamMaliyeti = value; }
+        }
+        public double calcUrunToplamMaliyeti()
+        {
+            
+            this.urunToplamMaliyeti = this.imalatMaliyeti + this.tasimaMaliyeti + this.depoMaliyeti;
+            return this.urunToplamMaliyeti;
+        }
+    }
+}
+
---
+++ supplyChain/supplyChain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supplyChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            supply order1 = new supply();
+            order1.SiparisNo = 1001;
+            order1.UrunId = 10;
+            order1.ImalatMaliyeti=2750;
+            order1.TasimaMaliyeti = 550;
+            order1.DepoMaliyeti = 990;
+            order1.calcUrunToplamMaliyeti();
+            Console.WriteLine(" Siparisi verilen urun toplam tutarı : {0}", order1.UrunToplamMaliyeti);
+            
+            Console.ReadLine();
+        }
+    }
+}
